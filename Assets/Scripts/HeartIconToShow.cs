a7b8fad6a0dea1f84316e62b6605deddc87d90ee
Nick Cummings <nickmcummings@gmail.com>
2016-10-08 12:05:57 -0700

    Working on UI display scripts

---
+++ Assets/Scripts/HeartIconToShow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartIconToShow : MonoBehaviour {
+
+	public int thisHeartHealth = 2;
+	public int thisHeartPlace;
+
+	public Sprite heartFull;
+	public Sprite heartHalfFull;
+	public Sprite heartEmpty;
+
+	SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (thisHeartHealth == 2) {
+			sr.sprite = heartFull;
+		} else if (thisHeartHealth == 1) {
+			sr.sprite = heartHalfFull;
+		} else if (thisHeartHealth == 0) {
+			sr.sprite = heartEmpty;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int playerHealth = 6;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
