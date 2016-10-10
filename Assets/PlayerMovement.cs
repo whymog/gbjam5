f050c0fbe861ffa27853b2002d9b5806fd97a9a8
Nick Cummings <nickmcummings@gmail.com>
2016-10-10 16:16:30 -0700

    Further animation logic work

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,10 +8,19 @@ public class PlayerMovement : MonoBehaviour {
 	private string playerFacingDirection = "down";
 	private bool playerIsMoving;
 
+	public Sprite playerFaceNorth;
+	public Sprite playerFaceEast;
+	public Sprite playerFaceSouth;
+	public Sprite playerFaceWest;
+
+	private SpriteRenderer sr;
+
 	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
+		sr = this.GetComponent<SpriteRenderer> ();
+
 		animator = this.GetComponent<Animator>();
 
 		playerIsStationary = true;
@@ -23,32 +32,44 @@ public class PlayerMovement : MonoBehaviour {
 		if (!Input.GetKey ("down") && !Input.GetKey ("up") && !Input.GetKey ("left") && !Input.GetKey ("right")) {
 			playerIsStationary = true;
 			animator.StopPlayback ();
+
+			if (playerFacingDirection == "down") {
+				sr.sprite = playerFaceSouth;
+			} else if (playerFacingDirection == "left") {
+				sr.sprite = playerFaceWest;
+			} else if (playerFacingDirection == "up") {
+				sr.sprite = playerFaceNorth;
+			} else if (playerFacingDirection == "right") {
+				sr.sprite = playerFaceEast;
+			}
 		}
 
 		if (Input.GetKey("down")) {
+			
+			animator.StartPlayback ();
 			transform.Translate(new Vector3 (0, -1, 0) * movementSpeedMultiplier);
-			if (playerIsStationary) {
+			if (playerFacingDirection != "down") {
 				SetPlayerDirection ("down");
 				animator.SetInteger("Direction", 0);
 			}
 		}
 		if (Input.GetKey ("up")) {
 			transform.Translate (new Vector3 (0, 1, 0) * movementSpeedMultiplier);
-			if (playerIsStationary) {
+			if (playerFacingDirection != "up") {
 				SetPlayerDirection ("up");
 				animator.SetInteger("Direction", 2);
 			}
 		}
 		if (Input.GetKey ("left")) {
 			transform.Translate (new Vector3 (-1, 0, 0) * movementSpeedMultiplier);
-			if (playerIsStationary) {
+			if (playerFacingDirection != "left") {
 				SetPlayerDirection ("left");
 				animator.SetInteger("Direction", 1);
 			}
 		}
 		if (Input.GetKey ("right")) {
 			transform.Translate (new Vector3 (1, 0, 0) * movementSpeedMultiplier);
-			if (playerIsStationary) {
+			if (playerFacingDirection != "right") {
 				SetPlayerDirection ("right");
 				animator.SetInteger("Direction", 3);
 			}
@@ -63,7 +84,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	void SetPlayerDirection(string dir) {
 		playerIsStationary = false;
-
+		Debug.Log ("turned" + playerFacingDirection);
 		playerFacingDirection = dir;
 	}
 }
