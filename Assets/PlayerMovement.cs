1797cc85c2479cf317139bd83a1d1e55545fcb80
Nick Cummings <nickmcummings@gmail.com>
2016-10-10 13:14:39 -0700

    Still working on player movement animations, even though I missed the deadline.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,8 +8,12 @@ public class PlayerMovement : MonoBehaviour {
 	private string playerFacingDirection = "down";
 	private bool playerIsMoving;
 
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
+		animator = this.GetComponent<Animator>();
+
 		playerIsStationary = true;
 		playerIsMoving = false;
 	}
@@ -18,30 +22,35 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
 		if (!Input.GetKey ("down") && !Input.GetKey ("up") && !Input.GetKey ("left") && !Input.GetKey ("right")) {
 			playerIsStationary = true;
+			animator.StopPlayback ();
 		}
 
 		if (Input.GetKey("down")) {
 			transform.Translate(new Vector3 (0, -1, 0) * movementSpeedMultiplier);
 			if (playerIsStationary) {
 				SetPlayerDirection ("down");
+				animator.SetInteger("Direction", 0);
 			}
 		}
 		if (Input.GetKey ("up")) {
 			transform.Translate (new Vector3 (0, 1, 0) * movementSpeedMultiplier);
 			if (playerIsStationary) {
 				SetPlayerDirection ("up");
+				animator.SetInteger("Direction", 2);
 			}
 		}
 		if (Input.GetKey ("left")) {
 			transform.Translate (new Vector3 (-1, 0, 0) * movementSpeedMultiplier);
 			if (playerIsStationary) {
 				SetPlayerDirection ("left");
+				animator.SetInteger("Direction", 1);
 			}
 		}
 		if (Input.GetKey ("right")) {
 			transform.Translate (new Vector3 (1, 0, 0) * movementSpeedMultiplier);
 			if (playerIsStationary) {
 				SetPlayerDirection ("right");
+				animator.SetInteger("Direction", 3);
 			}
 		}
 
